86a351348433815bb0d4a96d10aa6f64f6419bce
Pritom Chakma <chakmapritom1@gmail.com>
2026-08-23 22:40:58 +0600

    nonPrimitive

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,14 @@
+// Array,, Object
+
+let fruits : string[] = ["Mango", "Watermelon", "Orange", "Apple"]
+
+fruits.push("Banana", "Dargon")
+
+console.log(fruits);
+
+let mixedArray: (string | number)[] = ["Mango", 3, "Banans", 2, "Apple", 5]
+
+mixedArray.push("Milk", 1)
+
+let cordinate: [number, string] = [24, "Pritom"]
+
