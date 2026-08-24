e4558c4e74a90598af36e8e391604b3b113d6015
Pritom Chakma <chakmapritom1@gmail.com>
2026-08-25 01:53:57 +0600

    operators

---
+++ src/questionMark.ts
@@ -0,0 +1,15 @@
+// ?  : Ternery Operatoe
+//  ??  : Nullish Operator
+//  ?. : Optional Chaining
+
+
+const age = 24
+
+const myAge = age <= 24? "its my age" : "its  not my age"
+
+// console.log(myAge);
+
+const name = null
+
+const myName = name ?? "Pritom Chakma"
+console.log(myName);
